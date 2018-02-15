ed7fdf82f5a17bcfd4e33540027a7d52c03a437b
Joe Johnson <joe@Joes-MacBook-Pro.local>
2018-02-15 07:28:04 -0800

    CRD Routes.

--- balloon_time/urls.py
+++ balloon_time/urls.py
@@ -5,7 +5,7 @@ from . import views
 urlpatterns = [
     url(r'^$', views.party_list, name='party_list'),
     url(r'(?P<pk>\d+)/$', views.party_detail),
-    url(r'^post_party/$', views.post_party, name="post_party"),
+    url(r'^post_party/$', views.PostParty.as_view(), name="post_party"),
     url(r'/(?P<pk>[0-9]+)/$', views.PartyUpdate.as_view(), name='party_update'),
     url(r'/(?P<pk>[0-9]+)/delete/$', views.PartyDelete.as_view(), name='party_delete'),
 
--- balloon_time/views.py
+++ balloon_time/views.py
@@ -7,15 +7,32 @@ from django.urls import reverse_lazy
 
 
 def party_list(request):
-
+    parties = Party.objects.all()
+    form = PartyForm()
+    return render(request, 'party_list.html', {'parties': parties, 'form': form})
 
 def party_detail(request, pk):
+    party = Party.objects.get(pk=pk)
+    form = PartyForm()
+    return render(request, 'party_detail.html', {'party': party, 'form': form})
 
+# def post_party(request):
+#     # Refactor post_party to use CreateView class
+#     form = PartyForm(request.POST)
+#     if form.is_valid():
+#         party = form.save(commit = True)
+#     return HttpResponseRedirect('/')
 
-def post_party(request):
-
+class PostParty(CreateView):
+    model = Party
+    fields = ['title', 'location', 'description']
+    success_url = ('/')
 
 class PartyDelete(DeleteView):
-
+    model = Party
+    success_url = reverse_lazy('party_list')
 
 class PartyUpdate(UpdateView):
+    model = Party
+    fields = ['title', 'location', 'description']
+    template_name_suffix = 'Party_update_form'
